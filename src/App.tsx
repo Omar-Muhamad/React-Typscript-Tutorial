51477ae0093438ca1ee70771a526753b59aa173c
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2023-03-18 09:15:13 +0200

    Typing component state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Childern from './components/Childern';
+import Counter from './components/Counter';
 import NameBadge from './components/name-badge';
 
 const App = () => {
   return (
     <main className="application">
      {/* <NameBadge name='Omar' greeting='hello'/> */}
-     <Childern />
+     {/* <Childern /> */}
+     <Counter />
   </main>
   );
 }
